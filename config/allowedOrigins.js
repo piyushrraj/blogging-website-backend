81aa2d7671843eb7dcee340adf25e56e3fec1790
piyushrraj <piyushraj@piyushs-MacBook-Air.local>
2024-08-19 16:55:02 +0530

    fix error

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://super-gaufre-0f0ae3.netlify.app/'
+    'https://super-gaufre-0f0ae3.netlify.app'
 
 ];
 
